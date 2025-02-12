e26cac4b9eaedd2df97014eef43a26829429f71d
prieloizee <priscila.e.paulino@aluno.senai.br>
2025-02-12 13:35:49 -0300

    12-02

--- src/Cadastro/Cadastro.jsx
+++ src/Cadastro/Cadastro.jsx
@@ -1,98 +1,125 @@
 import * as React from "react";
-import TextField from "@mui/material/TextField"
-import Button from "@mui/material/Button"
-import Avatar from "@mui/material/Avatar"
-import  Box from "@mui/material/Box"
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
 import Person from "@mui/icons-material/Person";
-import Container from "@mui/material/Container"
-import Typography from "@mui/material/Typography"
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
 import { useState } from "react";
+import api from "../axios/axios"
 
-
-function Cadastro(){
+function Cadastro() {
 
     const[user, setUser] = useState({
+        cpf:"",
         email:"",
         password:"",
-        age:"",
         name:"",
+        data_nascimento:"",                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
     })
 
-    const onChange = (event) =>{
-        const{name, value} = event.target;
+    const onChange = (event) => {
+        const{ name , value} = event.target;
         setUser({...user,[name]:value})
     };
 
-    const handleSubmit=(event)=>{
+
+    const handleSubmit = (event) => {
         event.preventDefault();
-        alert("Email:"+user.email+""+"Senha:"+user.password+""+"Age:"+user.age+""+"Name:"+user.name+"")
+        Cadastro();
+    }
+    async function Cadastro(){
+        await api.postCadastro(user).then(
+            (response)=>{
+                alert(response.data.message)
+            },
+            (error)=>{
+                console.log(error)
+                alert(error.response.data.error)
+            }
+        )
     }
 
-    return(
-        <Container component="main" maxWidth="xs">
-            <Box sx={{
-                marginTop:8,
-                display:"flex",
-                flexDirection:"column",
-                alignItems:"center"
 
-            }}>
-                <Avatar sx={{margin:1, backgroundColor:"rgb(128,0,128)"}}>
-                    <Person/>
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Cadastro
-                </Typography>
-                <Box component="form" sx={{marginTop:1}} onSubmit={handleSubmit} noValidate>
-                    <TextField
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email"
-                    name="email"
-                    margin="normal"
-                    value={user.email}
-                    onChange ={onChange}
-                    />
-                    <TextField
-                    required
-                    fullWidth
-                    id="password"
-                    label="Senha"
-                    name="password"
-                    margin="normal"
-                    type="password"
-                    value={user.password}
-                    onChange ={onChange} 
-                    />
-                    <TextField
-                    required
-                    fullWidth
-                    id="age"
-                    label="Idade"
-                    name="age"
-                    margin="normal"
-                    value={user.age}
-                    onChange ={onChange} 
-                    />
-                    <TextField
-                    required
-                    fullWidth
-                    id="name"
-                    label="Nome"
-                    name="name"
-                    margin="normal"
-                    value={user.name}
-                    onChange ={onChange} 
-                    />
-                    <Button sx={{mt:3, mb:2, backgroundColor:"rgb(128,0,128)"}}
-                    fullWidth
-                    type="submit"
-                    variant="contained"
-                    >Entrar</Button>
-                </Box>
-            </Box>
-        </Container>
-    )
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box sx={{
+        marginTop:8,
+        display:"flex",
+        flexDirection:"column",
+        alignItems:"center"
+      }}>
+        <Avatar sx={{margin:1,backgroundColor:"rgb(128,0,128)"}}>
+          <Person />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Cadastro
+        </Typography>
+        <Box component="form" sx={{mt:1}} onSubmit={handleSubmit} noValidate>
+        <TextField 
+          required
+          fullWidth
+          id="cpf"
+          label="CPF"
+          name="cpf"
+          margin="normal"
+          value={user.cpf}
+          onChange={onChange}
+          />
+          <TextField 
+          required
+          fullWidth
+          id="email"
+          label="Email"
+          name="email"
+          margin="normal"
+          value={user.email}
+          onChange={onChange}
+          />
+          <TextField 
+            required
+            fullWidth
+            id="senha"
+            label="Senha"
+            name="password"
+            margin="normal"
+            type="password"
+            value={user.password}
+            onChange={onChange}
+            />
+
+             <TextField 
+          required
+          fullWidth
+          id="data_nascimento"
+          label="Data de nascimento"
+          name="data_nascimento"
+          margin="normal"
+          type="date"
+          value={user.data_nascimento}
+          onChange={onChange}
+          />
+
+        <TextField 
+          required
+          fullWidth
+          id="name"
+          label="Nome"
+          name="name"
+          margin="normal"
+          value={user.name}
+          onChange={onChange}
+          />
+          
+          <Button sx={{ mt:3, mb:2, backgroundColor:"rgb(128,0,128)"}}
+          fullWidth
+          type="submit"
+          variant="contained"
+          >Entrar</Button>
+        </Box> 
+      </Box>
+    </Container>
+  );
 }
 export default Cadastro;
